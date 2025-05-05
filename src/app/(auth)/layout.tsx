dff5038360dbc518a0b16e887c84014645737384
mukhtar4532 <mukhtarhussain4532@gmail.com>
2025-05-05 10:51:38 +0530

    learn link component, active links, params and searchparams, programmatically navigation and templates

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+const navigation = [
+  { title: "Login", href: "/login" },
+  { title: "Register", href: "/register" },
+];
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [inputVal, setInputVal] = useState("");
   return (
     <html lang="en">
       <body>
-        <div>Auth Header</div>
+        <div>
+          {navigation.map((nav) => {
+            return (
+              <Link key={nav.title} href={nav.href}>
+                {nav.title}
+              </Link>
+            );
+          })}
+        </div>
+        <input
+          type="text"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
         <div>{children}</div>
       </body>
     </html>
--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { Metadata } from "next";
+import { useRouter } from "next/navigation";
+import { use } from "react";
 
 type props = {
   params: Promise<{ productId: string }>;
@@ -6,15 +9,24 @@ type props = {
 
 // Define dynamic MetaData
 
-export async function generateMetaData({ params }: props): Promise<Metadata> {
-  const productId = (await params).productId;
-  return {
-    title: `Product ${productId}`,
-    description: "Product Description",
-  };
-}
+// export async function generateMetaData({ params }: props): Promise<Metadata> {
+//   const productId = (await params).productId;
+//   return {
+//     title: `Product ${productId}`,
+//     description: "Product Description",
+//   };
+// }
 
-export default async function ProductId({ params }: props) {
-  const productId = (await params).productId;
-  return <h1>Product Details {productId}</h1>;
+export default function ProductId({ params }: props) {
+  const productId = use(params).productId;
+  const router = useRouter();
+  const handleBuyNow = () => {
+    router.push("/");
+  };
+  return (
+    <div>
+      <h1>Product Details {productId}</h1>
+      <button onClick={handleBuyNow}>Buy Now</button>
+    </div>
+  );
 }
