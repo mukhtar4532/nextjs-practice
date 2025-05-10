a25de556358e8623028460bb5d4fc37d3710c471
mukhtar4532 <mukhtarhussain4532@gmail.com>
2025-05-10 22:36:34 +0530

    learn patch request

--- src/app/users/api/[id]/route.ts
+++ src/app/users/api/[id]/route.ts
@@ -8,3 +8,14 @@ export async function GET(
   const user = users.find((item) => item.id == Number(id));
   return Response.json(user);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const userRequest = await request.json();
+  const userIndex = users.findIndex((item) => item.id == Number(id));
+  users[userIndex]["name"] = userRequest.name;
+  return Response.json(users[userIndex]);
+}
