33f54ababeefdff723f0b4c26f3db0f7fb1ba43b
mukhtar4532 <mukhtarhussain4532@gmail.com>
2025-05-14 20:36:06 +0530

    learn cookie in route handler

--- src/app/test/route.ts
+++ src/app/test/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -8,9 +8,15 @@ export async function GET(request: NextRequest) {
   const reqHeader = await headers();
   console.log(reqHeader.get("abc"));
 
+  // const userCookie = request.cookies.get("test");
+  // console.log(userCookie);
+
+  const userCookie = await cookies();
+  console.log(userCookie.get("test"));
   return new Response("<h2> Hello next.js route handlers </h2> ", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "test = 12345678",
     },
   });
 }
