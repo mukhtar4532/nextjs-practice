df23a90b7eec704f1ac0c9dbf1472c47cdb9b4bf
mukhtar4532 <mukhtarhussain4532@gmail.com>
2025-05-14 20:08:51 +0530

    learn request and response headers

--- src/app/test/route.ts
+++ src/app/test/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-  return new Response("Hello next.js route handlers");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // const reqHeader = new Headers(request.headers)
+  // console.log(reqHeader.get('abc'))
+
+  const reqHeader = await headers();
+  console.log(reqHeader.get("abc"));
+
+  return new Response("<h2> Hello next.js route handlers </h2> ", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
